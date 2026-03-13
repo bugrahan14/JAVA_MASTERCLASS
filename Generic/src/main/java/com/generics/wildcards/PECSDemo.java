2bdffe61319eb677bea476fb9654bbdc161161f9
bugrahan14 <ertasbugra0611@gmail.com>
2026-03-13 15:13:24 +0300

    bug fixed

--- Generic/src/main/java/com/generics/wildcards/PECSDemo.java
+++ Generic/src/main/java/com/generics/wildcards/PECSDemo.java
@@ -2,6 +2,7 @@ package com.generics.wildcards;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,8 +10,8 @@ import java.util.List;
  */
 public class PECSDemo {
     public static void main(String[] args) {
-        List<Integer> ints = List.of(1, 2, 3);
-        List<Double> doubles = List.of(1.0, 2.0, 3.0);
+        List<Integer> ints = Arrays.asList(1, 2, 3);
+        List<Double> doubles = Arrays.asList(1.0, 2.0, 3.0);
 
         System.out.println("sum(ints): " + WildcardExamples.sum(ints));
         System.out.println("sum(doubles): " + WildcardExamples.sum(doubles));
